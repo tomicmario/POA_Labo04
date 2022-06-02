0f6aabe93431e3e22e9ab2c41dc8053723468c94
Mario <mario-tomic@hotmail.com>
2022-06-02 15:32:04 +0200

    main taking args

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
-
+#include <iostream>
 #include "graphics/Field.h"
 
-int main() {
+bool debug = true;
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    int fieldSize = 10;
+    int humanAmount = 10;
+    int vampireAmount = 10;
+    if(!debug) {
+        if (argc != 4) {
+            cout << "Expected arguments : [<field size>] [<human number>] [<vampire number>]" << endl;
+            return EXIT_SUCCESS;
+        }
+        fieldSize = atoi(argv[1]);
+        humanAmount = atoi(argv[2]);
+        vampireAmount = atoi(argv[3]);
+
+        if(fieldSize <= 0 || humanAmount <= 0 || vampireAmount <= 0){
+            cout << "All given arguments must be positive" << endl;
+            return EXIT_SUCCESS;
+        }
+        cout << "field size : " << fieldSize << endl
+             << "human amount : " << humanAmount << endl
+             << "vampire amount : " << vampireAmount << endl;
+    }
 
     Field f(10, 40);
 
     f.display();
 
-    return 0;
+    return EXIT_SUCCESS;
 }
