59728cefbfdae9a9ea3127b8667677318a4368de
Mario <mario-tomic@hotmail.com>
2022-06-09 14:45:19 +0200

    Slight fix random

--- humanoid/Human.cpp
+++ humanoid/Human.cpp
@@ -8,8 +8,11 @@ Human::Human(unsigned x, unsigned y): Humanoid(x,y){
 }
 
 void Human::setAction(Field &field) {
-    unsigned randomX = Util::getRandomUnsigned(0, field.getWidth());
-    unsigned randomY = Util::getRandomUnsigned(0, field.getHeight());
+
+    unsigned randomX = Util::getRandomUnsigned( (long)x - (long)speed < 0 ? 0: x - speed,
+            (long)x + (long)speed >= field.getWidth() ? field.getWidth() - 1: x + speed);
+    unsigned randomY = Util::getRandomUnsigned((long)y - (long)speed < 0 ? 0 : y - speed,
+    (long)y + (long)speed >= field.getHeight() ? field.getHeight() - 1 :  y + speed);
     ac = std::make_shared<Move>(shared_from_this(), randomX, randomY);
 }
 
